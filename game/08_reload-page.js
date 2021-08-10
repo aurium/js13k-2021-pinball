e61c430385cf19c386a3edf8be28588976e949e9
Aurélio A. Heckert <aurium@gmail.com>
2021-08-10 12:26:18 -0300

    Auto reload only on localhost

--- game/08_reload-page.js
+++ game/08_reload-page.js
@@ -1,12 +1,12 @@
 /* INI DEBUG */
-
-setInterval(()=> fetch('/update')
-  .then(response => response.text())
-  .then(update => {
-    const updateDate = new Date(update.trim().replace('_', 'T'))
-    const buildDate = new Date('#BUILD#'.replace('_', 'T'))
-    if (updateDate > buildDate) document.location.reload()
-  })
-  , 500)
-
+if (document.location.hostname === 'localhost') {
+  setInterval(()=> fetch('/update?t='+Date.now())
+    .then(response => response.text())
+    .then(update => {
+      const updateDate = new Date(update.trim().replace('_', 'T'))
+      const buildDate = new Date('#BUILD#'.replace('_', 'T'))
+      if (updateDate > buildDate) document.location.reload()
+    })
+    , 500)
+}
 /* END DEBUG */
