dfea4150de161e7b2d0e5943c153a5dd2dc638c8
Aurélio A. Heckert <aurium@gmail.com>
2021-09-01 21:42:14 -0300

    Finish "Limitless" level design

--- game/04_levels.js
+++ game/04_levels.js
@@ -14,7 +14,7 @@ const levels = [
       }
       const base = await mkBGStars(6, .52, 35, -128)
       // TODO: Write Blackhole and WormHole
-      // const base = ctxFloor.getImageData(0, 0, w, h)
+      // const base = getFloorImageData()
       // Create Frames:
       ctxFloor.lineWidth = u/2
       for (let f=1; f<7; f++) {
@@ -39,7 +39,7 @@ const levels = [
           if (f-1 === i || f+5 === i) plotChar(char, x,y, '#FFF', u/1.5)
           else plotChar(char, x,y, 'rgba(255,255,255,.5)', u/3)
         })
-        this.bg.push(ctxFloor.getImageData(0, 0, w, h))
+        this.bg.push(getFloorImageData())
       }
     },
     bgFreq: 300,
@@ -85,41 +85,48 @@ const levels = [
     async bg() {
       this.bg = []
       // Create base image:
-      const base = mkBGJulia(5, .511, -5, -24, 500, 150, 2.5, (x,y)=>
+      mkBGJulia(5, .511, -5, -24, 500, 150, 2.5, (x,y)=>
         `${hypotenuse(x-.5,y-.5)*500},100%,40%`
       )
+
+      ;[5,5,5,5,5,5,0].map(blur => {
+        ctxFloor.filter = `blur(${blur}px)`
+        ctxFloor.font = `bold ${3.5*u}px Arial, "Liberation Sans", sans-serif`
+        ctxFloor.textAlign = 'center'
+        ctxFloor.fillStyle = blur ? '#C0C' : '#FFF'
+        ctxFloor.fillText('Inferno', 10*u, 20*u)
+        ctxFloor.fillText('Clones',  90*u, 20*u)
+        ctxFloor.fillText('Mines',   10*u, 149*u)
+        ctxFloor.fillText('Solaris', 90*u, 149*u)
+      })
+      ctxFloor.filter = `none`
+
+      const base = getFloorImageData()
+
       // Create Frames:
       ctxFloor.lineWidth = u/2
-      for (let f=1; f<2; f++) {
+      for (let f=0; f<4; f++) {
         ctxFloor.putImageData(base, 0, 0)
 
-        ;[5,5,5,5,5,5,0].map(blur => {
-          ctxFloor.filter = `blur(${blur}px)`
-          ctxFloor.font = `bold ${3.5*u}px Arial, "Liberation Sans", sans-serif`
-          ctxFloor.textAlign = 'center'
-          ctxFloor.fillStyle = blur ? '#C0C' : '#FFF' //'#403'
-          ctxFloor.fillText('Inferno', 10*u, 20*u)
-          ctxFloor.fillText('Clones',  90*u, 20*u)
-          ctxFloor.fillText('Mines',   10*u, 149*u)
-          ctxFloor.fillText('Solaris', 90*u, 149*u)
-        })
-        ctxFloor.filter = `none`
-
-        // TODO: Remove it!
-        for (let i=1; i<6; i++) {
-          [
-            [50*vw + i*u*8, 50*vh],
-            [50*vw - i*u*8, 50*vh],
-            [50*vw, 50*vh + i*u*8],
-            [50*vw, 50*vh - i*u*8]
-          ].map(([x, y])=> {
-            let fill = f === i
-                     ? mkRadGrad(x,y,u/2, x,y,2*u, '#F20', '#800')
-                     : mkRadGrad(x,y,u/2, x,y,2*u, '#900', '#500')
-            drawCircle(ctxFloor, x, y, 2*u, fill)
+        // Draw blinking arrows
+        for (let i=0; i<4; i++) {
+          mapFor(PI/4, 2*PI, PI/2, (angle)=> {
+            let y = -i*14*u - 8*u
+            let gY = y - 3.7*u
+            ctxFloor.save()
+            ctxFloor.translate(50*vw, 50*vh)
+            ctxFloor.rotate(angle)
+            ctxFloor.font = `bold ${9*u}px Arial, "Liberation Sans", sans-serif`
+            ctxFloor.textAlign = 'center'
+            ctxFloor.fillStyle = (i === f)
+              ? mkRadGrad(0,gY,u/2, 0,gY,3*u, '#F20', '#800')
+              : mkRadGrad(0,gY,u/2, 0,gY,2*u, '#900', '#500')
+            ctxFloor.fillText('⬆', 0, y)
+            ctxFloor.restore()
           })
         }
-        this.bg.push(ctxFloor.getImageData(0, 0, w, h))
+
+        this.bg.push(getFloorImageData())
       }
     },
     bgFreq: 500,
@@ -129,21 +136,18 @@ const levels = [
              ball.y < 0 || ball.y > hMax
     },
     pins: [
-      [15, 25, 2, 4,  0,0,10],
-      [20, 20, 2, 4,  0,0,10],
-      [25, 15, 2, 4,  0,0,10],
-
-      [75, 15, 2, 4,  0,0,10],
-      [80, 20, 2, 4,  0,0,10],
-      [85, 25, 2, 4,  0,0,10],
-
-      [15, bottom(25), 2, 4,  0,0,10],
-      [20, bottom(20), 2, 4,  0,0,10],
-      [25, bottom(15), 2, 4,  0,0,10],
-
-      [75, bottom(15), 2, 4,  0,0,10],
-      [80, bottom(20), 2, 4,  0,0,10],
-      [85, bottom(25), 2, 4,  0,0,10]
+      ...mapFor(-15, 15, 5, (i)=>
+        [20-i, 20+i, 2, 4,  0,(i==15)?90:0,10, {i, wh:'inf', go:'sol'}]
+      ),
+      ...mapFor(-15, 15, 5, (i)=>
+        [80+i, 20+i, 2, 4,  0,(i==15)?90:0,10, {i, wh:'clo', go:'min'}]
+      ),
+      ...mapFor(-15, 15, 5, (i)=>
+        [20-i, bottom(20+i), 2, 4,  0,(i==15)?90:0,10, {i, wh:'min', go:'clo'}]
+      ),
+      ...mapFor(-15, 15, 5, (i)=>
+        [80+i, bottom(20+i), 2, 4,  0,(i==15)?90:0,10, {i, wh:'sol', go:'inf'}]
+      )
     ],
     wallsV: [],
     wallsH: [],
@@ -154,14 +158,38 @@ const levels = [
       [10, bottom(10), 6, 4],
       [90, bottom(10), 6, 5],
     ],
-    on: {}
+    on: {
+      colidePin(pin) {
+        const { i, go } = pin[7]
+        if (i == 15) { // Touch the red pin!
+          pin[7].i = 1 // Disable it
+          downPinProp(pin, 5) // Grayfy it
+          postPlay([1000, 0, .5, 1, 100])
+          const downPin = curLevel.pins.find(p => p[7].i == 0 && p[7].wh == go)
+          downPin[4] = downPin[5] = 90
+          lowerPin(downPin)
+        }
+      }
+    }
   },
 
   { /* * * LEVEL 2 * * */
     name: 'Inferno',
     info: 'all hope abandon ye who enter here',
     async bg() {
-      this.bg = []
+      return this.bg = []
+
+      ;[
+        [49*vw, 50*vh],
+        [19*vw, 42*vh],
+        [54*vw, 30*vh],
+        [80*vw, 62*vh],
+      ].map(([x, y], i)=> {
+        let fill = f === i
+                 ? mkRadGrad(x,y,u/2, x,y,2*u, '#F20', '#800')
+                 : mkRadGrad(x,y,u/2, x,y,2*u, '#900', '#500')
+        drawCircle(ctxFloor, x, y, 2*u, fill)
+      })
 
     },
     bgFreq: 500,
@@ -257,3 +285,11 @@ function lowerPin(pin) {
   if (pin[3] <= 0) pin[3] = 0
   else setTimeout(()=> lowerPin(pin), 60)
 }
+
+function downPinProp(pin, prop, to=0, step=2) {
+  pin[prop] -= step
+  if (pin[prop] <= to) pin[prop] = to
+  else setTimeout(()=> downPinProp(pin, prop, to, step), 60)
+}
+
+const getFloorImageData = ()=> ctxFloor.getImageData(0, 0, w, h)
